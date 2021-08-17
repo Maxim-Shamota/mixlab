2a6d517cea6fc44d7c2ac3373c2286e90440f094
Maxim <xbultazar@gmail.com>
2021-08-17 19:37:05 +0300

    add header and footer

---
+++ #src/js/files/script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    document.addEventListener("click", documentActions);
+
+    //Actions (делегирование события click)
+    function documentActions(e) {
+        const targetElement = e.targetElement;
+        
+    }
+}
