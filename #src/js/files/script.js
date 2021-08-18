f6795575fa84bdeae1bc64ddd56742f0d01b94b3
Maxim <xbultazar@gmail.com>
2021-08-18 21:33:56 +0300

    add 3 sections

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -6,4 +6,19 @@ window.onload = function () {
         const targetElement = e.targetElement;
         
     }
+
+    //Header
+    const headerElement = document.querySelector('.header');
+
+    const callback = function (entries, observer) {
+        if (entries[0].isIntersecting) {
+            headerElement.classList.remove('_scroll');
+        } else {
+            headerElement.classList.add('_scroll');
+        }
+    }
+
+    const headerObserver = new IntersectionObserver(callback);
+    headerObserver.observe(headerElement);
+
 }
